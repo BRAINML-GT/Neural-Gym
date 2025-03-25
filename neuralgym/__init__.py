8a918a72be5222899e813f2beae18196a7ec81cd
Feiyang Wu <feiyangwu@gatech.edu>
2025-03-25 18:53:56 -0400

    Add CustomReplayBuffer and utility functions for dopamine replay buffer management in __init__.py

--- neuralgym/__init__.py
+++ neuralgym/__init__.py
@@ -1,1 +1,16 @@
 __version__ = "0.1.0"
+from .replay_buffer import CustomReplayBuffer
+from .utils import (
+    create_dopamine_replay_buffer,
+    save_dopamine_replay_buffer,
+    load_dopamine_replay_buffer,
+)
+from .dopamine_level import DopamineEnv
+
+__all__ = [
+    "CustomReplayBuffer",
+    "DopamineEnv",
+    "create_dopamine_replay_buffer",
+    "save_dopamine_replay_buffer",
+    "load_dopamine_replay_buffer",
+]
